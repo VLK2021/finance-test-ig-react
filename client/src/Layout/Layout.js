fea503d11f28377648b80c03732f03a6ab031f58
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-07-18 11:18:59 +0300

    add sun moon

--- client/src/Layout/Layout.js
+++ client/src/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
+import {BsSun, BsMoonFill} from "react-icons/bs";
 
 import './LayoutStyle.css';
 import {ChangeIntervalComponent, FooterComponent, TitleComponent} from "../component";
@@ -8,13 +9,32 @@ import {ChangeIntervalComponent, FooterComponent, TitleComponent} from "../compo
 const Layout = () => {
     const currentTime = new Date().toLocaleDateString();
 
+    const onchange = () => {
+        document.body.classList.toggle("lightTherm")
+    }
+
 
     return (
         <div className={'layout width'}>
             <TitleComponent/>
 
             <div className={'time width flex'}>
-                {currentTime}
+                <div className={'sunMoon flex'}>
+                    <div className={'moon'}><BsMoonFill/></div>
+
+                    <div className="form-check form-switch toggle">
+                        <input type="checkbox"
+                               onChange={event => onchange(event.target.value)}
+                        />
+                    </div>
+
+                    <div className={'sun'}><BsSun/></div>
+                </div>
+
+
+                <div className={'flex currentTime'}>
+                    {currentTime}
+                </div>
             </div>
 
             <div className={'width'}>
